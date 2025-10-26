6f78b6e2e42b76c71b43bcbe669e4334ddc2f588
Rong "Mantle" Bao <webmaster@csmantle.top>
2025-10-26 20:40:27 +0800

    exp_2: add escape tokenizer

--- exp_2/exp_2.ml
+++ exp_2/exp_2.ml
@@ -1,20 +1,25 @@
 type token =
   | Char of char
-  | Closure
-  | Alt
+  | Star
+  | Plus
+  | Pipe
   | LParen
   | RParen
 
-let tokenize (s: string) : token list =
+let tokenize (s : string) : token list =
+  let len = String.length s in
   let rec aux i acc =
     if i >= String.length s then List.rev acc
     else
       match s.[i] with
-      | '*' -> aux (i + 1) (Closure :: acc)
-      | '|' -> aux (i + 1) (Alt :: acc)
+      | '*' -> aux (i + 1) (Star :: acc)
+      | '+' -> aux (i + 1) (Plus :: acc)
+      | '|' -> aux (i + 1) (Pipe :: acc)
       | '(' -> aux (i + 1) (LParen :: acc)
       | ')' -> aux (i + 1) (RParen :: acc)
-      | c   -> aux (i + 1) (Char c :: acc)
+      | '\\' ->
+          if i + 1 < len then aux (i + 2) (Char s.[i + 1] :: acc)
+          else raise (Invalid_argument "unterminated escape")
+      | c -> aux (i + 1) (Char c :: acc)
   in
   aux 0 []
-
