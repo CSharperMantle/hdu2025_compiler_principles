718e17ad0ee45b970580657bee6ba6c589766b3a
Rong "Mantle" Bao <webmaster@csmantle.top>
2025-10-27 11:19:00 +0800

    exp_2: add concat token explicitizer

--- exp_2/exp_2.ml
+++ exp_2/exp_2.ml
@@ -5,6 +5,7 @@ type token =
   | Pipe
   | LParen
   | RParen
+  | Concat (* implicit *)
 
 let tokenize (s : string) : token list =
   let len = String.length s in
@@ -19,7 +20,28 @@ let tokenize (s : string) : token list =
       | ')' -> aux (i + 1) (RParen :: acc)
       | '\\' ->
           if i + 1 < len then aux (i + 2) (Char s.[i + 1] :: acc)
-          else raise (Invalid_argument "unterminated escape")
+          else failwith "unterminated escape sequence"
       | c -> aux (i + 1) (Char c :: acc)
   in
   aux 0 []
+
+(*
+  Inserts concat token.
+  Examples:
+  * ab -> a ++ b
+  * a(b) -> a ++ (b)
+  * (a)(b) -> (a) ++ (b)
+  * a*b -> a* ++ b
+  * a+b -> a+ ++ b
+*)
+let explicitize_concat (tokens : token list) : token list =
+  let needs_concat_after = function Char _ | Star | Plus | RParen -> true | _ -> false
+  and needs_concat_before = function Char _ | LParen -> true | _ -> false in
+  let rec aux = function
+    | [] -> []
+    | [ x ] -> [ x ]
+    | x :: y :: rest ->
+        if needs_concat_after x && needs_concat_before y then x :: Concat :: aux (y :: rest)
+        else x :: aux (y :: rest)
+  in
+  aux tokens
