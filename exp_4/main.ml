2dc42fb7fa9ddbdc520cc7f46cff43de9573bec6
Rong "Mantle" Bao <webmaster@csmantle.top>
2025-11-25 13:56:59 +0800

    exp_4: add aggregated parsing error

--- exp_4/main.ml
+++ exp_4/main.ml
@@ -1,4 +1,6 @@
-exception Parsing_error of int * int * string
+open Parser.MenhirInterpreter
+
+type parsing_error = int * int * string
 
 let lex_file filename =
   let ic = open_in filename in
@@ -18,44 +20,48 @@ let lex_file filename =
     Printf.eprintf "Error type Lexer.Lexing_error at %s:%d:%d: %s\n" filename lineno colno msg;
     exit 1
 
+let parse (lexbuf : Lexing.lexbuf) : (Ast.comp_unit, parsing_error list) result =
+  let rec iterate errors checkpoint =
+    match checkpoint with
+    | InputNeeded _env ->
+        let token = Lexer.token lexbuf
+        and startp = lexbuf.lex_start_p
+        and endp = lexbuf.lex_curr_p in
+        iterate errors (offer checkpoint (token, startp, endp))
+    | Shifting _ | AboutToReduce _ -> iterate errors (resume checkpoint)
+    | HandlingError _env ->
+        let pos = Lexing.lexeme_start_p lexbuf in
+        (None, (pos.pos_lnum, pos.pos_cnum - pos.pos_bol + 1, "Generic error") :: errors)
+    | Accepted v -> (Some v, errors)
+    | Rejected ->
+        let pos = Lexing.lexeme_start_p lexbuf in
+        (None, (pos.pos_lnum, pos.pos_cnum - pos.pos_bol + 1, "Rejected") :: errors)
+  in
+  let checkpoint = Parser.Incremental.comp_unit lexbuf.lex_curr_p in
+  let comp_unit, errors = iterate [] checkpoint in
+  if errors <> [] then Error errors else Option.to_result ~none:errors comp_unit
+
 let parse_file filename =
   let ic = open_in filename in
   let lexbuf = Lexing.from_channel ic in
   lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = filename };
-
-  let rec iterate checkpoint : Ast.comp_unit =
-    match checkpoint with
-    | Parser.MenhirInterpreter.InputNeeded _env ->
-        let token = Lexer.token lexbuf
-        and startp = lexbuf.lex_start_p
-        and endp = lexbuf.lex_curr_p in
-        Parser.MenhirInterpreter.offer checkpoint (token, startp, endp) |> iterate
-    | Parser.MenhirInterpreter.Shifting _ | Parser.MenhirInterpreter.AboutToReduce _ ->
-        Parser.MenhirInterpreter.resume checkpoint |> iterate
-    | Parser.MenhirInterpreter.HandlingError _env ->
-        let pos = Lexing.lexeme_start_p lexbuf in
-        raise (Parsing_error (pos.pos_lnum, pos.pos_cnum - pos.pos_bol + 1, "Generic error"))
-    | Parser.MenhirInterpreter.Accepted v -> v
-    | Parser.MenhirInterpreter.Rejected ->
-        let pos = Lexing.lexeme_start_p lexbuf in
-        raise (Parsing_error (pos.pos_lnum, pos.pos_cnum - pos.pos_bol + 1, "Rejected"))
-  in
   try
-    let checkpoint = Parser.Incremental.comp_unit lexbuf.lex_curr_p in
-    let comp_unit = iterate checkpoint in
+    let comp_unit = parse lexbuf in
     close_in ic;
-    Ast.prettify_comp_unit comp_unit
-    |> List.fold_left (fun acc l -> acc ^ l ^ "\n") ""
-    |> print_endline
-  with
-  | Lexer.Lexing_error (lineno, colno, msg) ->
-      close_in ic;
-      Printf.eprintf "Error type Lexer.Lexing_error at %s:%d:%d: %s\n" filename lineno colno msg;
-      exit 1
-  | Parsing_error (lineno, colno, msg) ->
-      close_in ic;
-      Printf.eprintf "Error type Parsing_error at %s:%d:%d: %s\n" filename lineno colno msg;
-      exit 1
+    match comp_unit with
+    | Error errors ->
+        List.iter
+          (fun (lineno, colno, msg) ->
+            Printf.eprintf "Error type parsing_error at %s:%d:%d: %s\n" filename lineno colno msg)
+          errors
+    | Ok comp_unit ->
+        Ast.prettify_comp_unit comp_unit
+        |> List.fold_left (fun acc l -> acc ^ l ^ "\n") ""
+        |> print_endline
+  with Lexer.Lexing_error (lineno, colno, msg) ->
+    close_in ic;
+    Printf.eprintf "Error type Lexer.Lexing_error at %s:%d:%d: %s\n" filename lineno colno msg;
+    exit 1
 
 let usage () =
   Printf.eprintf "usage: %s <lex|parse> <source-file>\n" Sys.argv.(0);
